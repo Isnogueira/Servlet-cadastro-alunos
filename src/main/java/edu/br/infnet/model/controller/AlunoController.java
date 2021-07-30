d493d34470e817f15cce61aa31a4e5e0d05d0e10
Ingrid Nogueira <isnogueira601@gmail.com>
2021-07-30 08:00:34 -0300

    Consertei o erro do controller

--- src/main/java/edu/br/infnet/model/controller/AlunoController.java
+++ src/main/java/edu/br/infnet/model/controller/AlunoController.java
@@ -7,25 +7,23 @@ import javax.servlet.http.*;
 import java.io.IOException;
 
 public class AlunoController extends HttpServlet {
-/*
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("index.html").forward(request, response);
     }
-*/
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
 
-        Aluno aluno = new Aluno(req.getParameter("nome"), req.getParameter("email"));
-        aluno.setIdade(Integer.valueOf(req.getParameter("idade")));
-        aluno.setCurso(req.getParameter("curso"));
-        aluno.setMensalidade(Float.valueOf(req.getParameter("mensalidade")));
-        aluno.setRegiao(req.getParameter("regiao"));
-        aluno.setDisciplinas(req.getParameterValues("disciplina"));
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        Aluno aluno = new Aluno(request.getParameter("nome"), request.getParameter("email"));
+        aluno.setIdade(Integer.valueOf(request.getParameter("idade")));
+        aluno.setCurso(request.getParameter("curso"));
+        aluno.setMensalidade(Float.valueOf(request.getParameter("mensalidade")));
+        aluno.setRegiao(request.getParameter("regiao"));
+        aluno.setDisciplinas(request.getParameterValues("disciplina"));
 
         System.out.println(aluno);
-        req.getRequestDispatcher("confirmacao.html").forward(req, resp);
+        request.getRequestDispatcher("confirmacao.html").forward(request, response);
 
 
     }
